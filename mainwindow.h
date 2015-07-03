ee41b7e7a494693eae3f8413ceda149134896baa
Valentin Heinitz <vheinitz@googlemail.com>
2015-07-03 11:10:14 +0200

    Added project files

---
+++ mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QtGui/QMainWindow>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    enum ScreenOrientation {
+        ScreenOrientationLockPortrait,
+        ScreenOrientationLockLandscape,
+        ScreenOrientationAuto
+    };
+
+    explicit MainWindow(QWidget *parent = 0);
+    virtual ~MainWindow();
+
+    // Note that this will only have an effect on Symbian and Fremantle.
+    void setOrientation(ScreenOrientation orientation);
+
+    void showExpanded();
+
+private slots:
+    void on_actionExit_triggered();
+
+    void on_actionShow_Icon_triggered();
+
+    void on_actionShow_Icon_2_triggered();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
---
+++ updateqmake.bat
@@ -0,0 +1,8 @@
+@REM goes to all module-directories and calls qmake there
+@REM requires svn command line tool
+@REM Valentin Heinitz, 2011-08-27
+set PATH=C:\Qt\4.8.4\bin\
+
+
+qmake -t vcapp
+
